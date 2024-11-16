7ff743f89834bfdd39d921c1ca94c73c8740cd7d
Damian <damiancb97@gmail.com>
2024-11-17 00:31:13 +0100

    formulario sin grid

---
+++ js/formulario.js
@@ -0,0 +1,21 @@
+//Constantes con selección de ellas en html
+const loginTab = document.getElementById('login');
+const registerTab = document.getElementById('register');
+const loginForm = document.getElementById('login-form');
+const signupForm = document.getElementById('signup-form');
+
+//Evento para activar el Log IN
+loginTab.addEventListener('click', () => {
+    loginTab.classList.add('active');
+    registerTab.classList.remove('active');
+    loginForm.classList.add('active');
+    signupForm.classList.remove('active');
+});
+
+//Evento para activar Crear Cuenta
+registerTab.addEventListener('click', () => {
+    registerTab.classList.add('active');
+    loginTab.classList.remove('active');
+    signupForm.classList.add('active');
+    loginForm.classList.remove('active');
+});
